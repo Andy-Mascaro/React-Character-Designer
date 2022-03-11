df6264cf9234d1ad805f8f258c0fbe96d18d473d
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-11 12:37:05 -0800

    added functions to my phrase page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,13 +3,17 @@ import Bottom from '../Bottom/Bottom';
 import Head from '../Head/Head';
 import Middle from '../Middle/Middle';
 import Preview from '../../components/Preview/Preview';
+import Phrase from '../Phrase/Phrase';
 import './Main.css';
 
 
 export default function Main() {
-  const [head, setHead] = useState('head');
-  const [middle, setMiddle] = useState('middle');
+  const [head, setHead] = useState('dog');
+  const [middle, setMiddle] = useState('dress');
   const [bottom, setBottom] = useState('leg');
+  const [phrase, setPhrase] = useState('');
+  const [phraseArray, setPhraseArray] = useState([]);
+
   return (
     <main>
       <div>
@@ -19,7 +23,13 @@ export default function Main() {
           setMiddle={setMiddle}/>
         <Bottom
           setBottom={setBottom}/>
-         
+
+        <Phrase
+          setPhrase={setPhrase}
+          setPhraseArray={setPhraseArray}
+          phrase={phrase}
+          phraseArray={phraseArray}/>
+          
         <Preview 
           head={head}
           middle={middle}
--- src/components/Phrase/Phrase.js
+++ src/components/Phrase/Phrase.js
@@ -1,18 +1,22 @@
-// import React from 'react';
+import React from 'react';
 
-// export default function Phrase({ setAdd }) {
-//   return (
-//     <div className="phraseEditor">
-//     <label>Add A Catchphrase</label>
-//     <button>Add</button>
-//     onClick={(e) => {
-//         setAdd(e.target.value);
+export default function Phrase({ setPhrase, setPhraseArray, phrase, phraseArray }) {
+  const addPhrase = () => {setPhraseArray((prevState) => [...prevState, phrase]); setPhrase ('');};
+  return (
+    <div className="phraseEditor">
+      <label>Add A Catchphrase
+        <input value={phrase} onChange={(e) => setPhrase (e.target.value) } ></input>
+      </label>
+      <button onClick={addPhrase}>Add</button>
+      <ul> 
+        {phraseArray.map ((item) => <li key={item}>{item}</li>)}
 
-//     }}
-//    >
+      </ul>
+    
+  
 
 
 
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
